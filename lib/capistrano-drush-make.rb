9c64f08f870b9ca3dedb2209454e932e5a77dcbb
Christian Pearce <pearcec@xforty.com>
2012-02-13 13:39:57 -0500

    Initial version

--- lib/capistrano-drush-make.rb
+++ lib/capistrano-drush-make.rb
@@ -1,4 +1,5 @@
 require 'capistrano/recipes/deploy/strategy/remote_cache'
+require 'ash/drupal'
 
 module Capistrano
   module Deploy
@@ -31,6 +32,38 @@ module Capistrano
 
       end
 
+    end
+  end
+end
+
+
+configuration = Capistrano::Configuration.respond_to?(:instance) ?
+  Capistrano::Configuration.instance(:must_exist) :
+    Capistrano.configuration(:must_exist)
+
+configuration.load do
+
+  after "drupal:symlink","drupal:symlink_default_multisite"
+
+  namespace :deploy do
+    desc "Override ash, this is skipped"
+    task :setup_local do
+      # skip
+      logger.info %q<Overrode ash :setup_local system('cp .htaccess htaccess.dist')>
+    end
+  end
+
+  namespace :drupal do
+    desc "Symlink shared directories"
+    task :symlink, :roles => :web, :except => { :no_release => true } do
+      multisites.each_pair do |folder, url|
+        run "ln -nfs #{shared_path}/#{url} #{latest_release}/sites/#{url}"
+      end
+    end
+
+    desc "Symlink default to multisite"
+    task :symlink_default_multisite, :roles => :web, :except => { :no_release => true } do
+      run "rm -rf #{latest_release}/sites/default && ln -nfs #{latest_release}/sites/#{default_multisite} #{latest_release}/sites/default"
     end
   end
 end
